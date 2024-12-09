cf9857471d122758b7d324da8b9f8bf889f3532c
Abhishek Tiwari <91800088+abhishektiwari2003@users.noreply.github.com>
2024-12-09 16:30:20 +0530

    Fixed changes

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ import Footer from "./components/Footer";
 import Home from "./components/Home/Home";
 import SignIn from "./auth/Forms/SignIn";
 import SignUp from "./auth/Forms/SignUp";
-import StartupsList from "./components/Startup/StartupsList";
 
 // New Components (from Harsh-feature/dashboard-startup/investor)
 import Startup from "./components/Startup/Startup";
 import NewStartup from "./components/Startup/NewStartup";
 import ContactForm from "./components/Message/ContactForm";
 import Resources from "./components/Resources/Resources";
-<<<<<<< HEAD
 import StartupPage from "./components/Startup/StartupPage";
 import InvestorDashboard from "./components/Investor/InvestorDashboard";
 import InvestorPage from "./components/Investor/InvestorPage";
 import StartupDashboard from "./components/Startup/StartupDashboard";
-=======
-import Investor from "./components/Investor/Inverstor";
-import Founders from "./components/Startup/ForFunders/Founders";
->>>>>>> 815048467378330c3ece9fa3bc50efa91903963b
 
 const App = () => {
   return (
@@ -46,17 +40,10 @@ const App = () => {
           <Route path="/AddNewStartup" element={<NewStartup />} />
           <Route path="/message" element={<ContactForm />} />
           <Route path="/resources" element={<Resources />} />
-<<<<<<< HEAD
           <Route path="/StartupPage" element={<StartupPage />} />
           <Route path="/InvestorPage" element={<InvestorPage />} />
           <Route path="/InvestorDashboard" element={<InvestorDashboard />} />
           <Route path="/StartupDashboard" element={<StartupDashboard />} />
-=======
-          <Route path="/investor" element={<Investor/>}/>
-          <Route path="/startupsAll" element={<StartupsList/>}/>
-          <Route path="/forfounders" element={<Founders/>}/>
-
->>>>>>> 815048467378330c3ece9fa3bc50efa91903963b
           {/* Duplicate path "/startup-form" for clarity */}
           {/* <Route path="/startup-form" element={<NewStartup />} /> */}
         </Routes>
